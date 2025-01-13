a4e761af6bda2664c978a361421d4d25d6cdedfa
jackloosantamaria <jackloosantamaria@hotmail.com>
2025-01-12 19:23:58 -0500

    updated swagger

--- config/db.config.js
+++ config/db.config.js
@@ -1,5 +1,14 @@
+// const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 dotenv.config();
+
+// mongoose.connect(process.env.MONGODB_URI, { tls: true, tlsAllowInvalidCertificates: true })
+//   .then(() => {
+//     console.log('Connected to the database');
+//   })
+//   .catch((error) => {
+//     console.error('Cannot be connected', error);
+//   });
 
 module.exports = {
   url: process.env.MONGODB_URI,
--- index.js
+++ index.js
@@ -2,7 +2,15 @@ const express = require('express');
 const cors = require('cors');
 const app = express();
 
+
+const swaggerUi = require('swagger-ui-express');
+const swaggerDocument = require('./swagger.json');
+
+
+
+
 app
+  .use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument))
   .use(cors())
   .use(express.json())
   .use(express.urlencoded({ extended: true }))
@@ -26,3 +34,5 @@ const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}.`);
 });
+
+
